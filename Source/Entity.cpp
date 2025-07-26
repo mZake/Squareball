fe23e6c7d4ff226bff331752b0bcfcb4e4078e6f
mZake <zakeplaybr@gmail.com>
2025-07-26 13:56:59 -0300

    Fix player update function

--- Source/Entity.cpp
+++ Source/Entity.cpp
@@ -18,9 +18,20 @@ namespace Squareball
     void UpdatePlayer(Player& player, float delta)
     {
         Vector2 direction = {};
-        direction.x = (int)IsKeyDown(KEY_D) - (int)IsKeyDown(KEY_A);
-        direction.y = (int)IsKeyDown(KEY_S) - (int)IsKeyDown(KEY_W);
-        direction = Vector2Normalize(direction);
+
+        if (player.Controller == PlayerController::WASDKeys)
+        {
+            direction.x = (int)IsKeyDown(KEY_D) - (int)IsKeyDown(KEY_A);
+            direction.y = (int)IsKeyDown(KEY_S) - (int)IsKeyDown(KEY_W);
+        }
+        else if (player.Controller == PlayerController::ArrowKeys)
+        {
+            direction.x = (int)IsKeyDown(KEY_RIGHT) - (int)IsKeyDown(KEY_LEFT);
+            direction.y = (int)IsKeyDown(KEY_DOWN) - (int)IsKeyDown(KEY_UP);
+        }
+
+        if (direction != Vector2Zero())
+            direction = Vector2Normalize(direction);
 
         player.Velocity = direction * player.Speed * delta;
     }
