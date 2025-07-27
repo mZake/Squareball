165a44e606b576f4785cd1ce47d9e45cacac3976
mZake <zakeplaybr@gmail.com>
2025-07-27 16:07:43 -0300

    Enable window resizing

--- Source/Game.cpp
+++ Source/Game.cpp
@@ -1,15 +1,23 @@
 #include "Game.h"
 
+#include <cmath>
+
 #include <raylib.h>
 
 #include "Scenes/Match.h"
 
 namespace Squareball
 {
+    static Rectangle GetRenderTargetDestination();
+
+    constexpr int WindowWidth = 1152;
+    constexpr int WindowHeight = 640;
+
     struct Context
     {
-        UpdateCallback OnUpdate;
-        RenderCallback OnRender;
+        UpdateCallback  OnUpdate;
+        RenderCallback  OnRender;
+        RenderTexture2D RenderTarget;
     };
 
     static Context s_Context = {};
@@ -26,9 +34,13 @@ namespace Squareball
 
     void GameMain()
     {
-        InitWindow(1152, 640, "Squareball");
+        InitWindow(WindowWidth, WindowHeight, "Squareball");
+        SetWindowState(FLAG_WINDOW_RESIZABLE);
 
         InitMatchScene();
+
+        s_Context.RenderTarget = LoadRenderTexture(WindowWidth, WindowHeight);
+        SetTextureFilter(s_Context.RenderTarget.texture, TEXTURE_FILTER_POINT);
 
         while (!WindowShouldClose())
         {
@@ -36,11 +48,35 @@ namespace Squareball
 
             s_Context.OnUpdate(delta);
 
-            BeginDrawing();
+            BeginTextureMode(s_Context.RenderTarget);
             s_Context.OnRender();
+            EndTextureMode();
+
+            BeginDrawing();
+
+            ClearBackground(BLACK);
+
+            Texture2D targetTexture = s_Context.RenderTarget.texture;
+            Rectangle source = { 0.0f, 0.0f, (float)targetTexture.width, (float)-targetTexture.height };
+            Rectangle destination = GetRenderTargetDestination();
+            DrawTexturePro(targetTexture, source, destination, { 0.0f, 0.0f }, 0.0f, WHITE);
+
             EndDrawing();
         }
 
+        UnloadRenderTexture(s_Context.RenderTarget);
         CloseWindow();
+    }
+
+    static Rectangle GetRenderTargetDestination()
+    {
+        float scale = std::fminf((float)GetScreenWidth() / WindowWidth, (float)GetScreenHeight() / WindowHeight);
+
+        return Rectangle {
+            (GetScreenWidth() - ((float)WindowWidth * scale)) * 0.5f,
+            (GetScreenHeight() - ((float)WindowHeight * scale)) * 0.5f,
+            (float)WindowWidth * scale,
+            (float)WindowHeight * scale,
+        };
     }
 }
